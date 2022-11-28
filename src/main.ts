e99b0023946dd522038ebf0030af78ccb18ae238
AndreyGennadevich <andrermolaev@mail.ru>
2022-11-28 17:29:10 +0300

    initial commit

---
+++ src/main.ts
@@ -0,0 +1,42 @@
+import { Container, ContainerModule, interfaces } from 'inversify';
+import { App } from './app';
+import { ConfigService } from './config/config.service';
+import { IConfigService } from './config/config.service.interface';
+import { PrismaService } from './database/prisma.service';
+import { ExeptionFilter } from './errors/exeption.filter';
+import { IExeptionFilter } from './errors/exeption.filter.interface';
+import { ILogger } from './logger/logger.interface';
+import { LoggerService } from './logger/logger.service';
+import { TYPES } from './types';
+import { UserController } from './users/users.controller';
+import { IUserController } from './users/users.controller.interface';
+import { UsersRepository } from './users/users.repository';
+import { IUsersRepository } from './users/users.repository.interface';
+import { UsersService } from './users/users.service';
+import { IUsersService } from './users/users.service.interface';
+
+export interface IBootstrap {
+	appContainer: Container;
+	app: App;
+}
+
+export const appBindings = new ContainerModule((bind: interfaces.Bind) => {
+	bind<ILogger>(TYPES.ILogger).to(LoggerService).inSingletonScope();
+	bind<IExeptionFilter>(TYPES.ExeptionFilter).to(ExeptionFilter);
+	bind<IUserController>(TYPES.UserController).to(UserController);
+	bind<IUsersService>(TYPES.UsersService).to(UsersService);
+	bind<PrismaService>(TYPES.PrismaService).to(PrismaService).inSingletonScope();
+	bind<IConfigService>(TYPES.ConfigService).to(ConfigService).inSingletonScope();
+	bind<IUsersRepository>(TYPES.UsersRepository).to(UsersRepository).inSingletonScope();
+	bind<App>(TYPES.Application).to(App);
+});
+
+async function bootstrap(): Promise<IBootstrap> {
+	const appContainer = new Container();
+	appContainer.load(appBindings);
+	const app = appContainer.get<App>(TYPES.Application);
+	await app.init();
+	return { app, appContainer };
+}
+
+export const boot = bootstrap();
